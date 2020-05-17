f45775c04be8f0cb341be4b7f320f6908dbd0ea2
BayYan <click_g@hotmail.com>
2020-05-17 16:58:53 -0700

    try

--- R/hello_function.R
+++ R/hello_function.R
@@ -1,11 +1,13 @@
 #' A Hello Function
 #'
 #' This function allows you to say hello.
-#' @param text To whom you want to say hello.
+#' @param text_file_path file path to the text doc
 #' @keywords hello
 #' @export
 #' @examples
 #' hello_function()
-hello_function <- function(text) {
-	print(paste("hello", text))
+hello_function <- function(filename) {
+	droplist <- system.file("extdata", "droplist.txt", package = "senet")
+	print(paste("hello", filename))
+	print(paste("droplist: ", droplist))
 }
