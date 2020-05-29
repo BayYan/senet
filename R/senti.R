f5cba32be3c08723ba01ee8d267d465dd07592dc
BayYan <click_g@hotmail.com>
2020-05-29 16:40:15 -0700

    v1

--- R/senti.R
+++ R/senti.R
@@ -1,35 +1,114 @@
-#' SENET package in R
-#' This package reads text files and generates measures of text sentiments based on shortest path in the semantic network
-#' @param filename path to the text file
-#' @param window_size size of the n-gram
-#' @param useDroplist whether to drop a list of words, default is yes
-#' @param droplist path to the stopword list, if not specified, the built-in stopword list is used
+#' sentiment analysis
+#' @param edgelist edgelist with edges and counts
+#' @param target the target keyword string
+#' @param positiveLexicon path to the positive sentiment words in txt format, if not specified, the built-in lexicon is used
+#' @param negativeLexicon path to the negtive sentiment words in txt format, if not specified, the built-in lexicon is used
+#' @importFrom igraph graph.data.frame E V distances
 #' @export
-senti <- function(filename,window_size, useDroplist=TRUE, droplist=system.file("extdata", "droplist.txt", package = "senet")) {
-  
-  text <- unique(read.delim(filename, quote="", stringsAsFactor = FALSE,header=F, encoding="UTF-8")$V1)
-  doc <- quanteda::char_tolower(text)
-  doc <- gsub("\\w*[0-9]+\\w*\\s*", "",doc)
-  doc <- gsub("http.*", "", doc)
-  doc <- gsub("@|#|'|-|:", "", doc)
-  doc <- gsub("[^[:alnum:][:blank:]?&/\\-]", "", doc)
-  
-  #tokenize text
-  #remove punctuations, tokenize sentences
-  toks <- quanteda::tokens(doc, remove_punct = TRUE, remove_url = TRUE, remove_numbers = TRUE, remove_symbols = TRUE, remove_separators = TRUE)
-  
-  if (useDroplist) {
-    #remove stopwords 
-    drop <- read.delim(droplist, stringsAsFactor = FALSE,header=F)$V1
-    toks <- quanteda::tokens_select(toks, drop, selection = "remove")
-  }
-  
-  fcmat <- quanteda::fcm(toks, context = "window", window = window_size, ordered = TRUE)
 
-  g <- quanteda::as.igraph(fcmat,min_freq=3,omit_isolated=T)
-  
-  edgelist <- igraph::get.data.frame(g) 
-  edgelist$count <- 1
-  out <- aggregate(count~from+to,sum,data=edgelist)
-  return(out)
+senti <- function(edgelist, target, positiveLexicon=system.file("extdata", "SeedsPos.txt", package = "senet"), negativeLexicon=system.file("extdata", "SeedsNeg.txt", package = "senet")) {
+
+    graph <- igraph::graph.data.frame(edgelist)
+    igraph::E(graph)$weight <- igraph::E(graph)$count
+    
+    targetV <- igraph::V(graph)[name == target]
+     if (length(targetV) == 0){
+         stop("Target does not exist in document")}
+
+    positiveSenti <- list()
+    positiveDistances <- list()
+
+    positiveWords <- read.delim(positiveLexicon, stringsAsFactor = FALSE,header=F)$V1
+
+    for (positiveWord in positiveWords) {
+        posV <- igraph::V(graph)[name == positiveWord]
+        if (length(posV) == 0){next}
+        targetToPosMat <- igraph::distances(graph, v=targetV, to=posV, mode="out", algorithm = "dijkstra")
+        posToTargetMat <- igraph::distances(graph, v=posV, to=targetV, mode="out", algorithm = "dijkstra")
+       
+        if (targetToPosMat[1] != Inf && posToTargetMat[1] != Inf) {
+            dist <- targetToPosMat[1] + posToTargetMat[1]
+            positiveSenti <- append(positiveSenti, positiveWord)
+            positiveDistances <- append(positiveDistances, dist)
+        }
+    }
+
+    negativeSenti <- list()
+    negativeDistances <- list()
+
+    negativeWords <- read.delim(negativeLexicon, stringsAsFactor = FALSE,header=F)$V1
+
+    for (negativeWord in negativeWords) {
+        negV <- igraph::V(graph)[name == negativeWord]
+        if (length(negV) == 0){next}
+
+        targetToNegMat <- igraph::distances(graph, v=targetV, to=negV, mode="out", algorithm = "dijkstra")
+        negToTargetMat <- igraph::distances(graph, v=negV, to=targetV, mode="out", algorithm = "dijkstra")
+
+        if (targetToNegMat[1] != Inf && negToTargetMat[1] != Inf) {
+            dist <- targetToNegMat[1] + negToTargetMat[1]
+            negativeSenti <- append(negativeSenti, negativeWord)
+            negativeDistances <- append(negativeDistances, dist)
+        }
+    }
+
+    # print("positiveDistances")
+    # print(positiveDistances)
+
+    # print("negativeDistances")
+    # print(negativeDistances)
+
+    actualNumPositive = length(positiveDistances)
+    actualNumNegative = length(negativeDistances)
+
+    numPositive = length(positiveWords)
+    numNegative = length(negativeWords)
+
+    sumPositive = Reduce('+', positiveDistances)
+    sumNegative = Reduce('+', negativeDistances)
+
+    s1 <- 0
+    if (numPositive != 0) {
+        s1 <- sumPositive / numPositive
+    }
+
+    s2 <- 0
+    if (numNegative != 0) {
+        s2 <- sumNegative / numNegative
+    }
+
+    s3 <- 0
+    if (numNegative != 0) {
+        s3 <- s1 * numPositive / numNegative
+    }
+
+    s4 <- 0
+    if (s2 != 0) {
+        s4 <- s3 / s2
+    }
+
+    s5 <- 0
+    if (actualNumPositive != 0) {
+        s5 <- sumPositive / actualNumPositive
+    }
+
+    s6 <- 0
+    if (actualNumNegative != 0) {
+        s6 <- sumNegative / actualNumNegative
+    }
+
+    s7 <- 0
+    if (numNegative != 0) {
+        s7 <- s5 * numPositive / numNegative
+    }
+
+    s8 <- 0
+    if (s6 != 0) {
+        s8 <- s7 / s6
+    }
+
+    s9 <- s3 -s2
+
+    out <- as.data.frame(cbind(s1, s2, s3, s4, s5, s6, s7, s8, s9))
+    return(out)
 }
