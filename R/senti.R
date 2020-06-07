5dcefcf07879ef20c4bcea3aa07bee551a632dba
BayYan <click_g@hotmail.com>
2020-06-06 23:59:17 -0700

    handle multiple shortest paths

--- R/senti.R
+++ R/senti.R
@@ -3,7 +3,7 @@
 #' @param target the target keyword string
 #' @param positiveLexicon path to the positive sentiment words in txt format, if not specified, the built-in lexicon is used
 #' @param negativeLexicon path to the negtive sentiment words in txt format, if not specified, the built-in lexicon is used
-#' @importFrom igraph graph.data.frame E V shortest_paths
+#' @importFrom igraph graph.data.frame E V all_shortest_paths
 #' @export
 
 senti <- function(edgelist, target, positiveLexicon=system.file("extdata", "SeedsPos.txt", package = "senet"), negativeLexicon=system.file("extdata", "SeedsNeg.txt", package = "senet")) {
@@ -13,14 +13,25 @@ senti <- function(edgelist, target, positiveLexicon=system.file("extdata", "Seed
     graph <- igraph::graph.data.frame(edgelist)
 
     targetV <- igraph::V(graph)[name == target]
-     if (length(targetV) == 0){
-         stop("Target does not exist in document")}
+    if (length(targetV) == 0) {
+        stop("Target does not exist in document")
+    }
 
     nActualPositive <- 0
     sumPositive <- 0
 
     positiveWords <- read.delim(positiveLexicon, stringsAsFactor = FALSE,header=F)$V1
     negativeWords <- read.delim(negativeLexicon, stringsAsFactor = FALSE,header=F)$V1
+
+    PurePosCount=0
+    PurePosCountRev=0
+    PureNegCount=0
+    PureNegCountRev=0
+    nonPurePosCount=0
+    nonPurePosCountRev=0
+    nonPureNegCount=0
+    nonPureNegCountRev=0
+
 
     for (positiveWord in positiveWords) {
         posV <- igraph::V(graph)[name == positiveWord]
@@ -31,20 +42,24 @@ senti <- function(edgelist, target, positiveLexicon=system.file("extdata", "Seed
         result <- calculateDistance(graph, targetV, posV, negativeWords)
         targetToPosMat_weight <- result[1]
         targetToPosMat_length <- result[2]
+        nonPurePosCount <- nonPurePosCount + result[3]
 
         # reverse
         result <- calculateDistance(graph, posV, targetV, negativeWords)
         posToTargetMat_weight <- result[1]
         posToTargetMat_length <- result[2]
+        nonPurePosCountRev <- nonPurePosCountRev + result[3]
 
         if (targetToPosMat_length != 0) {
             dist_to <- targetToPosMat_weight / (targetToPosMat_length * targetToPosMat_length)
             sumPositive <- sumPositive + dist_to
+            PurePosCount <- PurePosCount + 1
         }
 
         if (posToTargetMat_length != 0) {
             dist_from <- posToTargetMat_weight / (posToTargetMat_length * posToTargetMat_length)
             sumPositive <- sumPositive + dist_from
+            PurePosCountRev <- PurePosCountRev + 1
         }
     }
 
@@ -56,26 +71,29 @@ senti <- function(edgelist, target, positiveLexicon=system.file("extdata", "Seed
         if (length(negV) == 0) { next }
         nActualNegative <- nActualNegative + 1
 
-        result <- calculateDistance(graph, targetV, negV, positiveWord)
+        result <- calculateDistance(graph, targetV, negV, positiveWords)
         targetToNegMat_weight <- result[1]
         targetToNegMat_length <- result[2]
+        nonPureNegCount <- nonPureNegCount + result[3]
 
         # reverse
-        result <- calculateDistance(graph, negV, targetV, positiveWord)
+        result <- calculateDistance(graph, negV, targetV, positiveWords)
         negToTargetMat_weight <- result[1]
         negToTargetMat_length <- result[2]
+        nonPureNegCountRev <- nonPureNegCountRev + result[3]
 
         if (targetToNegMat_length != 0) {
             dist_to <- targetToNegMat_weight / (targetToNegMat_length * targetToNegMat_length)
             sumNegative <- sumNegative + dist_to
+            PureNegCount <- PureNegCount + 1
         }
 
         if (negToTargetMat_length != 0) {
             dist_from <- negToTargetMat_weight / (negToTargetMat_length * negToTargetMat_length)
             sumNegative <- sumNegative + dist_from
+            PureNegCountRev <- PureNegCountRev + 1
         }
     }
-
 
     numPositive <- length(positiveWords)
     numNegative <- length(negativeWords)
@@ -142,21 +160,33 @@ senti <- function(edgelist, target, positiveLexicon=system.file("extdata", "Seed
 }
 
 calculateDistance <- function(graph, from, to, exclude) {
-    path <- igraph::shortest_paths(graph, from, to=to, mode="out", output="both")
-    pure <- TRUE
-    for (vertexIndex in path$vpath[[1]]) {
-        vertex <- V(graph)[vertexIndex]
-        if (vertex$name %in% exclude) {
-            pure <- FALSE
-            break
+    allPaths <- igraph::all_shortest_paths(graph, from, to=to, mode="out")
+
+    if (length(allPaths$res) == 0) {
+        return(c(0, 0, FALSE))
+    }
+    for (path in allPaths$res) {
+        pure <- TRUE
+        lastIndex <- -1
+        d <- 0
+        length <- 0
+        for (vertexIndex in path) {
+            vertex <- V(graph)[vertexIndex]
+            if (vertex$name %in% exclude) {
+                pure <- FALSE
+                break
+            }
+            if (lastIndex != -1) {
+                weight <- graph[lastIndex][vertexIndex][[1]]
+                d <- d + weight
+                length <- length + 1
+            }
+            lastIndex <- vertexIndex
+        }
+        if (pure) {
+            return(c(d, length, FALSE))
         }
     }
-    d <- 0
-    if (pure) {
-        for (edgeIndex in path$epath[[1]]) {
-            weight <- E(graph)$weight[edgeIndex]
-            d <- d + weight
-        }
-    }
-    return(c(d, length(path$epath[[1]])))
+
+    return(c(0, 0, TRUE))
 }
